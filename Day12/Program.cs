3b6ae1973d012ee1118b6ae65ddb1ce28069ede1
Emil Wall <emil.wall@who-umc.org>
2018-12-12 00:34:18 +0100

    Prepare day 12

---
+++ Day12/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+
+namespace Day12
+{
+    class Program
+    {
+        static readonly bool IsTest = true;
+        static readonly string[] Input = File.ReadAllLines(IsTest ? "example.txt" : "input.txt");
+
+        static void Main(string[] args)
+        {
+            var timer = Stopwatch.StartNew();
+
+            Console.WriteLine($"Part 1: , time: {timer.ElapsedMilliseconds}ms");
+
+            Console.WriteLine($"Part 2: , time: {timer.ElapsedMilliseconds}ms");
+        }
+    }
+}
