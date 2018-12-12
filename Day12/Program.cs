8ae75f8f4af2e7428accd29c4208c3f7bad13812
Emil Wall <emil.wall@who-umc.org>
2018-12-12 06:43:06 +0100

    Initial attempt at day 12

--- Day12/Program.cs
+++ Day12/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Diagnostics;
 using System.IO;
+using System.Linq;
 
 namespace Day12
 {
@@ -12,6 +13,17 @@ namespace Day12
         static void Main(string[] args)
         {
             var timer = Stopwatch.StartNew();
+            var state = "...." + Input.First().Substring(16) + "....";
+            var notes = Input.Skip(2).Select<string, Func<int, string, char?>>(line =>
+                (i, s) => line.Substring(0, 5) == s.Substring(i - 2, 5) ? (char?)line.ElementAt(9) : null
+            ).ToList();
+
+            for (var i = 0; i < 30; i++)
+            {
+                state = new string(Enumerable.Range(2, state.Length - 4).Select(j => notes.Select(note => note(j, state)).FirstOrDefault(c => c != null) ?? '.').ToArray());
+                // TODO Ensure minimum two . at both ends and keep track of indices
+                Console.WriteLine(state);
+            }
 
             Console.WriteLine($"Part 1: , time: {timer.ElapsedMilliseconds}ms");
 
