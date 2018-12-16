d88464ea4cb48ac60d9a4ce1d08bef5fe49d166f
emilwall <erif89@gmail.com>
2018-12-16 16:37:31 +0100

    Day 17 template

---
+++ Day17/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+
+namespace Day17
+{
+    class Program
+    {
+        private static readonly string[] Input = File.ReadAllLines("example.txt");
+
+        static void Main(string[] args)
+        {
+            var timer = Stopwatch.StartNew();
+
+            var parsed = Input.Select(line => line).ToList();
+
+            Console.WriteLine($"Finished in {timer.ElapsedMilliseconds}ms");
+            Console.WriteLine($"Part 1: {0}");
+            Console.WriteLine($"Part 2: {0}");
+        }
+    }
+}
