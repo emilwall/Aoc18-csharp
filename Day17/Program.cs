7b361bab1d77e4497e7516f5ce7c6ba23b9b1f6a
emilwall <erif89@gmail.com>
2018-12-17 09:37:15 +0100

    Day 17

--- Day17/Program.cs
+++ Day17/Program.cs
@@ -8,36 +8,46 @@ namespace Day17
 {
     class Program
     {
-        private static readonly string[] Input = File.ReadAllLines("example.txt");
+        private static readonly string[] Input = File.ReadAllLines("input.txt");
 
         static void Main(string[] args)
         {
             var timer = Stopwatch.StartNew();
 
+            var grid = GetGrid();
+            var waterTiles = grid.SelectMany(c => c).Count(c => new[] {'|', '~'}.Contains(c));
+            var stillWater = grid.SelectMany(c => c).Count(c => c == '~');
+
+            var finishTime = timer.ElapsedMilliseconds;
+            PrintGrid(grid);
+            Console.WriteLine($"Finished in {finishTime}ms");
+            Console.WriteLine($"Part 1: {waterTiles}");
+            Console.WriteLine($"Part 2: {stillWater}");
+        }
+
+        private static char[][] GetGrid()
+        {
             var waterPos = 500;
             var clay = GetClayLocations();
-            int xMin = clay.Min(c => c.x.Min()) - 1, xMax = clay.Max(c => c.x.Max()) + 1,
-                yMin = clay.Min(c => c.y.Min()), yMax = clay.Max(c => c.y.Max());
+            int xMin = clay.Min(c => c.Key) - 1,
+                xMax = clay.Max(c => c.Key) + 1,
+                yMin = clay.Min(c => c.Value.Min()),
+                yMax = clay.Max(c => c.Value.Max());
 
             var grid = Enumerable.Range(yMin, yMax - yMin + 1).Select(y =>
                 Enumerable.Range(xMin, xMax - xMin + 1).Select(x =>
-                    clay.Any(c => c.x.Contains(x) && c.y.Contains(y)) ? '#' : '.'
+                    clay.ContainsKey(x) && clay[x].Contains(y) ? '#' : '.'
                 ).ToArray()
             ).ToArray();
 
             grid[0][waterPos - xMin] = '|';
             FindBottomAndFill(grid, (x: waterPos - xMin, y: 0));
-            var waterTiles = grid.SelectMany(c => c).Count(c => new[] {'|', '~'}.Contains(c));
 
-            Console.WriteLine($"Finished in {timer.ElapsedMilliseconds}ms");
-            PrintGrid(grid);
-            Console.WriteLine($"Part 1: {waterTiles}");
-            Console.WriteLine($"Part 2: {0}");
+            return grid;
         }
 
         private static void FindBottomAndFill(char[][] grid, (int x, int y) water)
         {
-            var initial = water.y;
             while (water.y < grid.Length && grid[water.y][water.x] != '#')
             {
                 grid[water.y++][water.x] = '|';
@@ -49,18 +59,29 @@ namespace Day17
                 do
                 {
                     grid[--water.y][water.x] = '~';
-                    left = water.x;
-                    while (grid[water.y][left - 1] != '#' && new[] {'#', '~'}.Contains(grid[water.y + 1][left]))
+                    left = right = water.x;
+                    while (left > -1
+                           && (left == 0 || grid[water.y][left - 1] != '#')
+                           && (water.y + 1 == grid.Length || new[] {'#', '~'}.Contains(grid[water.y + 1][left])))
                     {
                         grid[water.y][--left] = '~';
                     }
 
-                    right = water.x;
-                    while (grid[water.y][right + 1] != '#' && new[] {'#', '~'}.Contains(grid[water.y + 1][right]))
+                    while (right < grid[0].Length + 1
+                           && (right + 1 == grid[0].Length || grid[water.y][right + 1] != '#')
+                           && (water.y + 1 == grid.Length || new[] {'#', '~'}.Contains(grid[water.y + 1][right])))
                     {
                         grid[water.y][++right] = '~';
                     }
-                } while (grid[water.y][left - 1] == '#' && grid[water.y][right + 1] == '#');
+                } while (left > 0
+                         && right + 1 < grid[0].Length
+                         && grid[water.y][left - 1] == '#'
+                         && grid[water.y][right + 1] == '#');
+
+                for (var i = left; i <= right; i++)
+                {
+                    grid[water.y][i] = '|';
+                }
 
                 if (grid[water.y + 1][left] == '.')
                 {
@@ -74,20 +95,23 @@ namespace Day17
             }
         }
 
-        private static List<(List<int> x, List<int> y)> GetClayLocations()
+        private static Dictionary<int, HashSet<int>> GetClayLocations()
         {
-            return Input.Select(line =>
+            return Input.SelectMany(line =>
             {
-                int xStart = 2, xEnd = line.IndexOf(','),
-                    y1Start = xEnd + 4, y1End = line.IndexOf('.'),
-                    y2Start = y1End + 2;
-                var x = int.Parse(line.Substring(xStart, xEnd - xStart));
-                var y1 = int.Parse(line.Substring(y1Start, y1End - y1Start));
-                var y2 = int.Parse(line.Substring(y2Start));
-                var single = Enumerable.Range(x, 1).ToList();
-                var range = Enumerable.Range(y1, y2 - y1 + 1).ToList();
-                return line.StartsWith('x') ? (single, range) : (range, single);
-            }).ToList();
+                int end1 = line.IndexOf(','), end2 = line.IndexOf('.'),
+                    first = int.Parse(line.Substring(2, end1 - 2)),
+                    second = int.Parse(line.Substring(end1 + 4, end2 - end1 - 4)),
+                    third = int.Parse(line.Substring(end2 + 2));
+                var range = Enumerable.Range(second, third - second + 1).ToList();
+                return line.StartsWith('x')
+                    ? new[] { (x: first, ys: range) }
+                    : range.Select(x => (x: x, ys: new List<int> { first }));
+            }).GroupBy(pair => pair.x)
+                .ToDictionary(
+                    group => group.Key,
+                    group => new HashSet<int>(group.SelectMany(pair => pair.ys))
+                );
         }
 
         private static void PrintGrid(char[][] grid)
