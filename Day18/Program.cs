57c055eaa97f42a9f61fc3b90b9f1039894aa797
emilwall <erif89@gmail.com>
2018-12-18 07:56:10 +0100

    Day 18 Parallelized instead of alternating between two grids

--- Day18/Program.cs
+++ Day18/Program.cs
@@ -8,24 +8,23 @@ namespace Day18
     class Program
     {
         private static readonly string[] Input = File.ReadAllLines("input.txt");
-        private static readonly char[][] Even = Input.Select(line => line.ToCharArray()).ToArray();
-        private static readonly char[][] Odd = Input.Select(line => line.ToCharArray()).ToArray();
-        private static int minute;
+        private static char[][] _grid = Input.Select(line => line.ToCharArray()).ToArray();
+        private static int _minute;
 
         static void Main(string[] args)
         {
             var timer = Stopwatch.StartNew();
 
             Execute(10);
-            var res1 = Even.SelectMany(c => c).Count(c => c == '|') *
-                       Even.SelectMany(c => c).Count(c => c == '#');
+            var res1 = _grid.SelectMany(c => c).Count(c => c == '|') *
+                       _grid.SelectMany(c => c).Count(c => c == '#');
 
-            Execute(608); // Found through manual inspection and some math, will differ depending on input
-            var res2 = Even.SelectMany(c => c).Count(c => c == '|') *
-                       Even.SelectMany(c => c).Count(c => c == '#');
+            Execute(607); // Found through manual inspection and some math, will differ depending on input
+            var res2 = _grid.SelectMany(c => c).Count(c => c == '|') *
+                       _grid.SelectMany(c => c).Count(c => c == '#');
 
             var finishTime = timer.ElapsedMilliseconds;
-            Visualize(Odd);
+            Visualize(_grid);
             Console.WriteLine($"Finished in {finishTime}ms");
             Console.WriteLine($"Part 1: {res1}");
             Console.WriteLine($"Part 2: {res2}");
@@ -38,18 +37,16 @@ namespace Day18
 
         private static void Execute(int targetMinute)
         {
-            while (minute++ <= targetMinute)
+            while (_minute++ <= targetMinute)
             {
-                var next = minute % 2 == 0 ? Even : Odd;
-                var prev = next == Even ? Odd : Even;
-                for (var i = 0; i < next.Length; i++)
-                {
-                    for (var k = 0; k < next[0].Length; k++)
-                    {
-                        next[i][k] = Evolve(prev, i, k);
-                    }
-                }
+                _grid = _grid.AsParallel().Select((row, i) =>
+                    row.Select((c, k) =>
+                        Evolve(_grid, i, k)
+                    ).ToArray()
+                ).ToArray();
             }
+
+            _minute--;
         }
 
         private static char Evolve(char[][] prev, int i, int k)
